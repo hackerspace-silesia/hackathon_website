5fa131ba0c4e0a77869968fe783feab28e9e8a8e
unknown <vkamil.kolasinski@gmail.com>
2017-09-09 12:45:24 +0200

    addded cordova camera preview

--- src/pages/new-advert/new-advert.ts
+++ src/pages/new-advert/new-advert.ts
@@ -36,7 +36,6 @@ export class NewAdvertPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewAdvertPage');
   }
 
   createAdvert() {
@@ -65,8 +64,18 @@ export class NewAdvertPage {
   popCallback = (_param) => {
     return Promise.resolve(_param)
     .then(url => {
-      this.img = url;
+      let file = this.dataURLtoFile(_param, 'test');
+      this.img = file;
     });
+  }
+
+  dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
   }
 
   addPhoto() {
