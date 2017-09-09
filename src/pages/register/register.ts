9ed282874494badee48440d60c3289912e01aeb0
mrzepiel <marcinrzepiel@wp.pl>
2017-09-09 10:25:06 +0200

    Create simply validation

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import validator from 'validator';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthServiceProvider } from "../../providers/auth-service/auth-service";
 import { HomePage } from "../home/home";
@@ -18,12 +19,12 @@ import { AlertController } from "ionic-angular";
 })
 export class RegisterPage {
   password: string = '';
-  error: string = '';
   firstName: string = '';
   lastName: string = '';
   email: string = '';
   nip: number = null;
   registerSuccess: boolean = false;
+  error: object;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
@@ -45,18 +46,41 @@ export class RegisterPage {
         nip: this.nip
       }
     };
-    this.authService.register(credentials)
-      .subscribe(result => {
-        if(result === true) {
+
+    if (this.password.length < 8) {
+      this.error = {
+        type: 'Validation Error',
+        message: 'Minimal password lenght is 8',
+      };
+      return
+    } else if (this.firstName.length === 0) {
+      this.error = {
+        type: 'Validation Error',
+        message: 'First name is required',
+      };
+      return
+    } else if (this.lastName.length === 0) {
+      this.error = {
+        type: 'Validation Error',
+        message: 'Last name is required',
+      };
+      return;
+    } else if (this.email.length === 0) {
+      this.error = {
+        type: 'Validation Error',
+        message: 'Email is required',
+      };
+      return;
+    } else {
+      this.authService.register(credentials)
+        .subscribe(result => {
           this.registerSuccess = true;
           this.password = '';
           this.firstName = '';
           this.lastName = '';
           this.email = '';
           this.nip = null;
-        } else {
-          this.error = 'Email lub hasło jest niepoprawne';
-        }
-      })
+        })
+    }
   }
 }
